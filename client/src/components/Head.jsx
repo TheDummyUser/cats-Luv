5b1662d8fbb6a5fdc0a1399e7d9270810648a932
TheDummyUser <abhiram.reddy122002@gmail.com>
2024-03-21 20:52:18 +0530

    added header

--- client/src/components/Head.jsx
+++ client/src/components/Head.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom'
 const Head = () => {
   return (
     <>
-    <div>
-        <p>Cats and Luv</p>
-        <p><Link to='/dashboard'>dashboard</Link></p>
+    <div className='flex space-x-4 p-6 justify-end border-b-2 border-slate-900'>
+        <p className='border border-black p-1 pl-3 pr-3 rounded-2xl bg-slate-700 text-slate-300'><Link to='/'>Cats and Luv</Link></p>
+        <p className='border border-black p-1 pl-3 pr-3 rounded-2xl bg-slate-700 text-slate-300'><Link to='/dashboard'>dashboard</Link></p>
     </div>
     </>
   )
